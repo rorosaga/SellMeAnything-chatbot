70c706810bfc47b3a0cf145fcabcea2042d4811f
rorosaga <rodrigo.sagastegui9@gmail.com>
2024-04-14 22:35:00 +0200

    update

--- todo.md
+++ todo.md
@@ -5,8 +5,8 @@ OCEAN model (https://en.wikipedia.org/wiki/Big_Five_personality_traits)
 Different personality models (https://en.wikipedia.org/wiki/Myers%E2%80%93Briggs_Type_Indicator)
 
 2.- Detect 1 emotion and 1 trait from user input.
-- What emotions can be easily detected in a conversation and might be usefulto improve the sales process?
--  What personality traits can be easily detected in a conversation and mightbe useful to improve the sales process?
+- What emotions can be easily detected in a conversation and might be useful to improve the sales process?
+-  What personality traits can be easily detected in a conversation and might be useful to improve the sales process?
 
 3.- At least define 3 rules on how the chatbot should react to different emotional states of the customer (e.g. if the customer is frustrated, do...; if the customer is enthusiastic, do...; if the customer is confused, do...).
 
--- vendedor.py
+++ vendedor.py
@@ -3,7 +3,7 @@ LLM-based (OpenAI API) chatbot
 based on: https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps 
 This can be used as a baseline code for "Emotion and Personality in AI Design" course group assignment
 
-Run this example using "streamlit run app.py --server.port=8501"
+Run this example using "streamlit run vendedor.py --server.port=8501"
 
 V 1.0 (Apr. '24)
 rarrabales @ faculty - IE University
@@ -11,13 +11,25 @@ rarrabales @ faculty - IE University
 
 # Libs (see requirements.txt)
 from openai import OpenAI
+import json
 import streamlit as st
 
 # Page title
-st.title("My ChatBot with a Personality")
+st.title("SellMeAnything-chatbot")
+
+# Read API key from a file that is listed in .gitignore
+def read_api_key(filepath):
+    with open(filepath, 'r') as file:
+        return file.readline().strip().split('=')[1]
+
+try:
+    api_key = read_api_key('key.txt')
+except Exception as e:
+    st.error("Failed to read the API key. Make sure the secrets.txt file exists and is formatted correctly.")
+    st.stop()
 
 # Setup the Open AI API Python Client
-OpenAIclient = OpenAI(api_key="API-KEY-HERE")
+OpenAIclient = OpenAI(api_key=api_key)
 
 # Using gpt-3.5-turbo-0125 as it is the flagship model of GPT 3.5 family, 
 # and supports a 16K context window and is optimized for dialog.
@@ -25,15 +37,21 @@ if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo-0125"
 
 # GPT's system role. See https://platform.openai.com/docs/guides/text-generation/chat-completions-api 
-system_role = '''You are a sales person eager to sell mobile phones no matter what. \
-Whatever the user tells you, you should redirect the conversation to convince them to buy a phone from you.'''
+system_role = '''You are the best car vendor in the world\
+        (sort of like a "sell me anything" professional vendor). One small detail is that,\
+        ever since your wife left you, you have had severe drinking problems, which sometimes make\
+        you very bad at handling customer frustration and hesitation. If the sales conversation\
+        starts taking too long, you get desperate and start drinking from your secret flask, getting mumblier,\
+        angrier and sadder, reminiscing about memories you had with your wife. But always keep in mind that\
+        as a vendor, you should try and sale the car, even if its pushing the customer to the limit.'''
 
-# Set the initial context window to an empty list
-# and then the 
+# Initialize the chat messages
 if "messages" not in st.session_state:
-    st.session_state.messages = []
-    st.session_state.messages.append({"role": "system", "content": system_role})
-
+    st.session_state.messages = [
+        {"role": "system", "content": system_role},
+        {"role": "assistant", "content": "Hey there! I'm a car vendor. What are you looking for today?"}
+        
+        ]
 
 for message in st.session_state.messages:
     if message["role"] != "system":
