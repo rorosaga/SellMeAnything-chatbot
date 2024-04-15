714009e555f61c755b014b0410ac36b61b4448c7
ramcav <110005362+ramcav@users.noreply.github.com>
2024-04-15 17:50:45 +0200

    Fixed issues with .gitignore, created JSON, fixed formatting of vendedor.py

--- vendedor.py
+++ vendedor.py
@@ -18,7 +18,7 @@ import csv
 
 def main():
     # Page title
-    st.title("SellMeAnything-chatbot")
+    st.title("Your Personal Ferrari Car Vendor")
 
     try:
         api_key = read_api_key('key.txt')
@@ -29,8 +29,10 @@ def main():
     # Setup the Open AI API Python Client
     OpenAIclient = OpenAI(api_key=api_key)
     
+    # Setup the prompt
     setup_prompt()
     
+    # Call the chatbot
     call_chatbot_for_chat(OpenAIclient)
 
 # Read API key from a file that is listed in .gitignore
@@ -38,35 +40,24 @@ def read_api_key(filepath):
     with open(filepath, 'r') as file:
         return file.readline().strip().split('=')[1]
     
-
-
+# Setup the chatbot prompt
 def setup_prompt():
     # Using gpt-3.5-turbo-0125 as it is the flagship model of GPT 3.5 family, 
     # and supports a 16K context window and is optimized for dialog.
     if "openai_model" not in st.session_state:
         st.session_state["openai_model"] = "gpt-3.5-turbo-0125"
         
-    # CAR INVENTORY LIST FOR CHATBOT CONTEXT
-    car_inventory = {
-        "cars": [
-            {"brand": "Ferrari", "model": "SF90 Stradale", "year": 2022, "speed": "340 km/h", "engine": "4.0L V8 Hybrid", "price": "593,750 EUR"},
-            {"brand": "Ferrari", "model": "488 Pista", "year": 2020, "speed": "340 km/h", "engine": "3.9L V8 Turbo", "price": "350,000 EUR"},
-            {"brand": "Ferrari", "model": "F8 Tributo", "year": 2022, "speed": "340 km/h", "engine": "3.9L V8 Turbo", "price": "262,000 EUR"},
-            {"brand": "Ferrari", "model": "Roma", "year": 2022, "speed": "320 km/h", "engine": "3.9L V8 Turbo", "price": "200,000 EUR"},
-            {"brand": "Ferrari", "model": "Portofino M", "year": 2021, "speed": "320 km/h", "engine": "3.9L V8 Turbo", "price": "215,000 EUR"},
-            {"brand": "Ferrari", "model": "812 Superfast", "year": 2022, "speed": "340 km/h", "engine": "6.5L V12", "price": "337,000 EUR"},
-            {"brand": "Ferrari", "model": "812 GTS", "year": 2022, "speed": "340 km/h", "engine": "6.5L V12", "price": "366,000 EUR"},
-            {"brand": "Ferrari", "model": "Monza SP1", "year": 2020, "speed": "300 km/h", "engine": "6.5L V12", "price": "1,600,000 EUR"},
-            {"brand": "Ferrari", "model": "Monza SP2", "year": 2020, "speed": "300 km/h", "engine": "6.5L V12", "price": "1,600,000 EUR"},
-            {"brand": "Ferrari", "model": "LaFerrari", "year": 2015, "speed": "352 km/h", "engine": "6.3L V12 Hybrid", "price": "2,500,000 EUR"}
-        ]
-    }
-
+    # Read the JSON from a file
+    with open('vehicles.json', 'r') as file:
+        car_inventory = json.load(file)
+        
+    # TURN JSON INTO A STRING FORMAT FOR PROMPT
     car_descriptions = "\n".join([
     f"{car['brand']} {car['model']} ({car['year']}): Priced at {car['price']}, top speed {car['speed']}. Features include {car['engine']} engine."
     for car in car_inventory['cars']
     ])
 
+    # List of emotions the chatbot should consider and how to hanld them
     emotions = '''
             Frustration: When a customer shows frustration, you  should try to remain calm and empathetic, despite your own struggles with handling similar emotions. You should quickly offer solutions or alternatives to address the customer’s concerns, perhaps suggesting a different vehicle that better meets their needs or offering a test drive to ease their decision-making process,
 
@@ -105,7 +96,7 @@ def setup_prompt():
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
     
-
+# Call the chatbot
 def call_chatbot_for_chat(client):
     if prompt := st.chat_input("your message"):
         st.session_state.messages.append({"role": "user", "content": prompt})
@@ -124,17 +115,24 @@ def call_chatbot_for_chat(client):
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
         
-        personality_trait, emotion = detect_emotion_and_trait(prompt, client)
+        # Process the user input in the background to get the personality trait and emotion
+        # exhibited in each message
+        emotion, personality_trait = detect_emotion_and_trait(prompt, client)
+        
         # Log the interaction without updating the UI
-        log_to_csv(prompt, personality_trait, emotion)
+        log_to_csv(prompt, emotion, personality_trait)
         
 def detect_emotion_and_trait(text, client):
-    emotion_list = ["frustration", "excitement", "indecision", "curiosity", "indecision", "impatience"]
+    
+    # List of emotions
+    emotion_list = ["frustration", "excitement", "indecision", "curiosity","impatience", "sadness"]
     emotions_string = ', '.join(emotion_list)
     
+    # List of personality traits acc to OCEAN model
     ocean_list = ["Openness", "Conscientiousness", "Extraversion", "Agreeableness", "Neuroticism"]
     ocean_string = ', '.join(ocean_list)
     
+    # GPT's system role
     system_role = f'''
         You are an expert psychologist, and you are analyzing the personality traits and emotions of people based on their text.\
         You have from the following emotions in your repertoire: {emotions_string} and the following personality traits: {ocean_list}\
@@ -150,28 +148,21 @@ def detect_emotion_and_trait(text, client):
       stream = False
     )
 
-    # Extract words (assuming the API response is well-formatted)
+    # Extract words from the response
     words = response.choices[0].message.content.strip().split()
+    
     # Return personality trait and emotion
     if len(words) >= 2:
-        return words[0], words[1]  # Assuming the first word is the trait, second is the emotion
+        return words[0], words[1]  # First word is emotion, second word is personality trait
     else:
-        return "Unknown", "Unknown"
+        return "Unknown", "Unknown" # Return unknown if the response is not as expected
     
-def log_to_csv(user_input, personality_trait, emotion):
+# Log the interaction to a CSV file
+def log_to_csv(user_input, emotion, personality_trait):
     with open('user_interactions.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([datetime.datetime.now(), user_input, personality_trait, emotion])
-        
-def process_user_input(prompt, OpenAIclient):
-    # This function simulates a background process handling the chat input
-    personality_trait, emotion = detect_emotion_and_trait(prompt, OpenAIclient)
-
-    # Log the interaction without any UI updates
-    log_to_csv(prompt, personality_trait, emotion)
-    
-    return
-
+        # Write the current date and time, user input, personality trait, and emotion to the CSV file
+        writer.writerow([datetime.datetime.now(), user_input, emotion, personality_trait])
     
         
 if __name__ == "__main__":
